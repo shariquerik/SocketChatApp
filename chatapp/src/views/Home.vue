78ff363b6ee4b91ec54afd08bf294129e2efa86d
Shariq <sharique.rik@gmail.com>
2020-09-28 13:16:36 +0530

    Updated some static content

--- chatapp/src/views/Home.vue
+++ chatapp/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home block mt-10 sm:flex">
     <div class="m-2 p-4">
       <div class="text-indigo-400 font-semibold text-3xl mb-4">Welcome to my Live Chat Room App.</div>
-      <div class="text-indigo-200 break-words text-lg">This app is made using Socket.io, Vuejs, Expressjs and Nodejs. Enter you name and room id provided by your admin and Enjoy chating!!!</div>
+      <div class="text-indigo-200 break-words text-lg">This app is made using Socket.io, Vuejs, Expressjs, Nodejs and MongoDB. Enter your name in 'USERNAME' and create or join room by entering room id in 'ROOMID'. Enjoy chating!!!</div>
     </div>
     <div class="p-5 m-5 bg-indigo-200 rounded-lg">
       <div class="text-xl mb-2 font-bold text-indigo-600">Live Chat Room</div>
@@ -13,7 +13,7 @@
         </div>
         <div class="flex flex-col mb-4">
           <label for="roomid" class="font-bold text-indigo-600 text-xs">ROOMID<span class="text-red-600">*</span></label>
-          <input class="rounded-lg focus:outline-none px-3 py-1" type="text" id="roomid" v-model="roomid" required placeholder="Enter room id">
+          <input class="rounded-lg focus:outline-none px-3 py-1" type="text" id="roomid" v-model="roomid" required placeholder="Enter room id or create one">
         </div>
         <a type="submit" :href="'/#/chatroom/' + this.username + '/' + this.roomid" class="bg-indigo-500 rounded-lg text-white px-2 py-1 w-full text-center">Enter Room</a>
       </form>
